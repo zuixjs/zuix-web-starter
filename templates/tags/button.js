a3067e7c570b776891dc605d50582e79275625a9
Gene <genemars@users.noreply.github.com>
2022-02-12 00:21:15 +0100

    fixed 'mini-fab' button renderHeight

--- templates/tags/button.js
+++ templates/tags/button.js
@@ -7,7 +7,7 @@ module.exports = (render, content, linkUrl, buttonType, buttonClass) => {
   // buttonClass :==  'mini-fab' | 'accent' | 'colored' | 'primary'
   let renderHeight = 36;
   if (buttonType === 'fab') {
-    renderHeight = buttonClass === 'mini-fab' ? 40 : 56;
+    renderHeight = buttonClass.indexOf('mini-fab') !== -1 ? 40 : 56;
   }
   return render(template, {content, linkUrl, buttonType, buttonClass, renderHeight});
 };
