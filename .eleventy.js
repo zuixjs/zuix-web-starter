2eeafb0f8043a03a95f99a845302e84f52ca72e8
Gene <genemars@users.noreply.github.com>
2022-03-11 12:09:41 +0100

    Implemented internal watcher for detecting removed files and folder to trigger rebuild. Optimized .eleventy.js and .eleventy-zuix.js code.

--- .eleventy.js
+++ .eleventy.js
@@ -27,29 +27,14 @@
  */
 
 const path = require('path');
-const config = require('config');
-const util = require('util');
 const compress = require('compression');
-const chokidar = require('chokidar');
 
 // 11ty
 const {EleventyRenderPlugin} = require("@11ty/eleventy");
 
-// zuix.js CLI utils
-const zuixCompile = require('zuix-cli/commands/compile-page');
-const zuixUtils = require('zuix-cli/common/utils');
-
-// Read configuration either from './config/{default}.json'
-// or './config/production.json' based on current `NODE_ENV'
-// environment variable value
-let zuixConfig = config.get('zuix');
-const sourceFolder = zuixConfig.get('build.input');
-const buildFolder = zuixConfig.get('build.output');
-const dataFolder = zuixConfig.get('build.dataFolder');
-const includesFolder = zuixConfig.get('build.includesFolder');
-const copyFiles = zuixConfig.get('build.copy');
-const ignoreFiles = zuixConfig.get('build.ignore');
-const componentsFolders = zuixConfig.get('build.componentsFolders');
+// zuix.js
+const zuix11ty = require('./.eleventy-zuix');
+const zuixConfig = zuix11ty.getConfig();
 
 // LESS CSS compiler
 const less = require('less');
@@ -62,38 +47,23 @@ const lintConfig = require(process.cwd() + '/.eslintrc');
 
 // Minifier
 //const { minify } = require("terser");
-const fs = require('fs');
-const {render} = require('template-file');
 
 // Keep track of changed files for zUIx.js post-processing
-const postProcessFiles = [];
-const changedFiles = [];
 let browserSync;
-let rebuildAll = true;
-// - copy last zUIx release
-zuixUtils.copyFolder(util.format('%s/node_modules/zuix-dist/js', process.cwd()), util.format('%s/js/zuix', buildFolder), (err) => {
-  if (err) console.log(err);
-});
-// - auto-generated config.js
-zuixUtils.generateAppConfig(zuixConfig);
-// replace {{variables}} used in the config
-zuixConfig = JSON.parse(render(JSON.stringify(zuixConfig), zuixConfig));
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.setWatchJavaScriptDependencies(false);
   eleventyConfig.addPlugin(EleventyRenderPlugin);
 
   // Add ignores
-  [...ignoreFiles, ...componentsFolders].forEach((f) => {
-    f = path.join(sourceFolder, f);
+  [...zuixConfig.ignoreFiles, ...zuixConfig.componentsFolders].forEach((f) => {
+    f = path.join(zuixConfig.sourceFolder, f);
     eleventyConfig.ignores.add(f);
-    console.log('Adding ignore "%s"', f);
   });
-  // Copy base files
-  copyFiles.forEach((f) => {
-    f = path.join(sourceFolder, f);
-    eleventyConfig.addPassthroughCopy(f);
-    console.log('Adding copy "%s"', f);
+  // Ignore "copy" files, because they are handled by zuix11ty
+  zuixConfig.copyFiles.forEach((f) => {
+    f = path.join(zuixConfig.sourceFolder, f);
+    eleventyConfig.ignores.add(f);
   });
 
   // from https://github.com/kkgthb/web-site-11ty-03-netlify-function/blob/main/.eleventy.js
@@ -142,7 +112,7 @@ module.exports = function(eleventyConfig) {
   // Add any BrowserSync config option here
   eleventyConfig.setBrowserSyncConfig({
     //reloadDelay: 2000,
-    files: [ ...componentsFolders ],
+    files: [ ...zuixConfig.componentsFolders ],
     notify: false,
     cors: true,
     middleware: [compress()],
@@ -150,6 +120,8 @@ module.exports = function(eleventyConfig) {
       ready: function(err, bs) {
         // store a local reference of BrowserSync object
         browserSync = bs;
+        // setup zuix-11ty watcher
+        zuix11ty.setupWatcher(eleventyConfig, browserSync.publicInstance);
       }
     },
     /*
@@ -164,99 +136,16 @@ module.exports = function(eleventyConfig) {
     }*/
   });
 
-
-  // zUIx.js specific code and life-cycle hooks
-  eleventyConfig.addGlobalData("app", zuixConfig.app);
-  // Add zUIx transform
-  eleventyConfig.addTransform('zuix-js', function(content) {
-    const inputPath = this.inputPath;
-    const outputPath = this.outputPath;
-    const hasChanged = changedFiles.find(f => path.resolve(f) === path.resolve(inputPath));
-    if (!rebuildAll && !hasChanged) return content;
-    // populates a list of `.html` files
-    // to be post processed after build
-    if (outputPath && outputPath.endsWith('.html')) {
-      let file = path.resolve(outputPath);
-      const baseFolder = path.resolve(zuixConfig.build.output);
-      if (file.startsWith(baseFolder)) {
-        file = file.substring(baseFolder.length + 1);
-      }
-      postProcessFiles.push({file, baseFolder: zuixConfig.build.output});
-    }
-    return content;
-  });
-  eleventyConfig.on('beforeWatch', (cf) => {
-    // changedFiles is an array of files that changed
-    // to trigger the watch/serve build
-    changedFiles.length = 0;
-    const baseFolder = path.resolve(zuixConfig.build.input);
-    const dataFolder = path.join(baseFolder, zuixConfig.build.dataFolder);
-    const includesFolder = path.join(baseFolder, zuixConfig.build.includesFolder);
-    const templateChanged = cf.find(f => path.resolve(f).startsWith(includesFolder));
-    const dataChanged = cf.find(f => path.resolve(f).startsWith(dataFolder));
-    if (templateChanged || dataChanged) {
-      rebuildAll = true;
-      return;
-    }
-    console.log(cf);
-    changedFiles.push(...cf);
-  });
-  eleventyConfig.on('afterBuild', async function(args) {
-    console.log();
-    postProcessFiles.forEach((pf) => {
-      const result = zuixCompile(pf.file, pf.file, {
-        baseFolder: pf.baseFolder,
-        ...zuixConfig
-      });
-      // TODO: check result code and report
-    });
-    postProcessFiles.length = 0;
-    if (zuixConfig.build.serviceWorker) {
-      console.log('\nUpdating Service Worker... ');
-      await zuixUtils.generateServiceWorker().then(function () {
-        console.log('... Service Worker updated.');
-      });
-    } else {
-      console.log();
-    }
-    if (rebuildAll) {
-      // reverts to incremental build mode
-      rebuildAll = false;
-    }
-  });
-  // Watch zuix.js folders (`./templates` and `./source/app`) not watched by 11ty
-  eleventyConfig.addWatchTarget('./templates/tags/');
-  const watchEvents = {add: true, change: true, unlink: true};
-  chokidar.watch(componentsFolders.map(p =>  path.resolve(path.join(sourceFolder, p)))).on('all', (event, file) => {
-    if (watchEvents[event] && fs.existsSync(file)) {
-      const outputFile = path.resolve(path.join(buildFolder, file.substring(path.resolve(sourceFolder).length)));
-      const outputFolder = path.dirname(outputFile);
-      if (!fs.existsSync(outputFolder)) {
-        fs.mkdirSync(outputFolder, { recursive: true })
-      }
-      fs.copyFileSync(file, outputFile);
-    } else {
-      // TODO: maybe remove file from output folder as well?
-    }
-    if (browserSync) {
-      browserSync.publicInstance.reload();
-    }
-  });
-
-
-  // integrate custom user config with a dedicated
-  // `eleventy-config.js` module file
-  require('./.eleventy-zuix')(eleventyConfig);
-
+  zuix11ty.configure(eleventyConfig);
 
   // Return 11ty configuration options:
   return {
-    pathPrefix: zuixConfig.app.baseUrl,
+    pathPrefix: zuixConfig.baseUrl,
     dir: {
-      input: sourceFolder,
-      output: buildFolder,
-      data: dataFolder,
-      includes: includesFolder,
+      input: zuixConfig.sourceFolder,
+      output: zuixConfig.buildFolder,
+      data: zuixConfig.dataFolder,
+      includes: zuixConfig.includesFolder,
       layouts: "_inc/layouts"
     },
     //htmlTemplateEngine: false, // 'liquid'
