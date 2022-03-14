4968e7b63067a14fbd2ab7a7a2a96d7e28842b4f
Gene <genemars@users.noreply.github.com>
2022-03-14 23:57:24 +0100

    Reduced layout shift.

--- templates/tags/card.js
+++ templates/tags/card.js
@@ -1,8 +1,8 @@
 const template = `
-<div view z-load="@lib/templates/mdl-card{{ type }}" class="visible-on-ready" style="margin: 12px;min-width: {{ minWidth }}px; min-height: {{ minHeight }}px; display: inline-block">
+<div view z-load="@lib/templates/mdl-card{{ type }}" class="visible-on-ready" style="margin: 12px;min-width: {{ minWidth }}px; min-height: {{ minHeight }}px">
   <h1 #title style="font-size: 150%!important; position: absolute; padding: 12px" class="mdl-color-text--primary-contrast">{{ title }}</h1>
-  <img #image src="{{ image }}" alt="Cover image" role="presentation">
-  <div style="position: absolute; bottom: 0; width: 100%; background: #ffffffAA">
+  <img #image src="{{ image }}" alt="Cover image" role="presentation" width="100%">
+  <div style="position: absolute; bottom: 0; background: #ffffffAA">
     <p #text>{{ content }}</p>
     <a #link.url href="{{ linkUrl }}">
       <span #link.title>{{ linkTitle }}</span>
