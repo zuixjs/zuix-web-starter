8cdf7c60147b2d8abb9f9e91b760c0ff06ba1c19
gene <generoso.martello@gmail.com>
2025-05-11 00:37:51 +0200

    Upgraded to zuix-cli 0.5.4 and Eleventy 3.0.

--- docs/config.js
+++ docs/config.js
@@ -13,11 +13,4 @@
     },
     "siteMapUrl": "https://zuixjs.github.io/zuix-web-starter/"
   });
-  // Check that service workers are registered
-  if ('serviceWorker' in navigator) {
-    // Use the window load event to keep the page load performant
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/zuix-web-starter/service-worker.js');
-    });
-  }
 })();
